9525b445705e3c689d259614fbadfb75fbbc0c51
N4ims <79645839+N4ims@users.noreply.github.com>
2023-05-07 20:28:48 +0200

    Update setup_tables.sql

--- setup_tables.sql
+++ setup_tables.sql
@@ -110,33 +110,3 @@ ALTER TABLE `room_type_amenities` ADD FOREIGN KEY (`amenity_id`) REFERENCES `ame
 ALTER TABLE `catering_bookings` ADD FOREIGN KEY (`catering_type_id`) REFERENCES `catering_types` (`id`);
 
 ALTER TABLE `catering_bookings` ADD FOREIGN KEY (`room_booking_id`) REFERENCES `room_bookings` (`id`);
-
-
-
--- Fill tables
-
-INSERT INTO addresses VALUES (2, 'Brentonstreet', "1337", '69111', 'Mannheim', 'Germany');
-
-INSERT INTO guests (id, first_name, last_name, birthdate, address_id, telephone_number, member_since) VALUES (1, 'Günther', 'Hellmann', '1963-05-05', 2, '06623 4466', '2023-04-20');
-INSERT INTO guests (id, first_name, last_name, birthdate, address_id, email_address, member_since) VALUES (2, 'Maxima', 'Musterfrau', '1999-06-16', 2, 'maxima.musterfrau@gmail.com', '2023-04-18');
-
-INSERT INTO room_types VALUES (2, 'Doppelzimmer luxus', 'Unser luxuriöses Doppelzimmer', 'king size Boxspringbett', 2, 4, 25000, 45);
-INSERT INTO room_types VALUES (1, 'Einzelzimmer nomal', 'Unser kuscheliges Standardzimmer mit einem Einzelbett', 'normales Einzelbett 120cm', 1, 1, 10000, 18);
-
-INSERT INTO rooms VALUES (1, 1);
-INSERT INTO rooms VALUES (2, 1);
-INSERT INTO rooms VALUES (3, 2);
-
-INSERT INTO amenities VALUES (1, 'Klimaanlage', '');
-INSERT INTO amenities VALUES (2, 'Fußbodebheizung', '');
-
-INSERT INTO room_type_amenities VALUES (1, 2, 1);
-INSERT INTO room_type_amenities VALUES (2, 2, 2);
-INSERT INTO room_type_amenities VALUES (3, 1, 2);
-
-INSERT INTO room_bookings VALUES (1, 2, 1, '2023-05-01', '2023-05-10', 2, 1, '2023-05-01 20:55:20', 'nice customer');
-INSERT INTO room_bookings VALUES (2, 1, 1, '2023-06-01', '2023-06-10', 2, 1, '2023-05-01 20:55:20', 'nice customer');
-
-INSERT INTO catering_types VALUES (1, 'Frühstück', 'Frühstück von unserem wundervollen Frühstücksbuffet. Von 7:00 bis 10:30 Uhr.', 12);
-INSERT INTO catering_types VALUES (2, 'Halbpension', 'Frühstück und Abendessen. Frühstück von 7:00 Uhr bis 10:30 Uhr. Abendessen von 18:00 Uhr bis 21 Uhr.', 30);
-INSERT INTO catering_types VALUES (3, 'Vollpension', 'Frühstück und Abendessen. Frühstück von 7:00 Uhr bis 10:30 Uhr. Mittagessen von 11:30 Uhr bis 14 Uhr. Abendessen von 18:00 Uhr bis 21 Uhr.', 45);
